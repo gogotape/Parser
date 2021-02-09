3366d517a60215027a4976010f133a990d371736
Misha <bobovmikhail@mail.ru>
2021-02-09 11:21:26 +0300

    empty parser + defs. third commit

--- lib/parser_file.py
+++ lib/parser_file.py
@@ -17,3 +17,45 @@ def transform_schedule(keywords, parameters, input_file, output_file):
     pass
 
 
+def read_schedule():
+    pass
+
+
+def inspect_schedule():
+    pass
+
+
+def clean_schedule():
+    pass
+
+
+def parse_schedule():
+    pass
+
+
+def extract_keyword_blocks():
+    pass
+
+
+def extract_lines_from_keyword_block():
+    pass
+
+
+def parse_keyword_block():
+    pass
+
+
+def parse_keyword_DATE_line():
+    pass
+
+
+def parse_keyword_COMPTDAT_line():
+    pass
+
+
+def parse_keyword_COMPTDATL_line():
+    pass
+
+
+def result_to_csv():
+    pass
