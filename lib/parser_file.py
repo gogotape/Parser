f0bf82801fe90b956920f2ef7c520daf924e52d4
Misha <bobovmikhail@mail.ru>
2021-03-01 21:13:42 +0300

    unsuccessful attempt of adding def format_eclipse_string. 5 commit

--- lib/parser_file.py
+++ lib/parser_file.py
@@ -5,6 +5,7 @@ import numpy
 import pandas
 E:/code/Parser_project/input_data/test_schedule.inc
 'W1' 10 10 1 3 OPEN 1* 1 2 1 3* 1.0 /
+'W3' 'LGR1' 10 10  2   2 	OPEN 	1* 	1	2 	1 	3* 			1.0918 /
 """
 
 input_f = input('Введите абсолютный путь к .inc файлу: ')
@@ -73,20 +74,32 @@ def parse_keyword_COMPDATL_line():
     print(out[:-1])
 
 
+def format_eclipse_string():
+    s = input('Введите строку для форматирования значений DEFAULT: ')
+    old_string = s
+    def f(match):
+        return str(int(match.group(3))+1)
+
+    L = re.sub('([0-9])+', f, old_string)
+    print(L)
+
+
 def result_to_csv():
     pass
 
 
-clean_schedule()
 
-a.close()
+
 a = open('file.txt', encoding="utf-8")
 
-parse_keyword_DATE_line()
 '''
+parse_keyword_DATE_line()
 parse_keyword_COMPDAT_line()
 parse_keyword_COMPDATL_line()
 '''
+
+
+format_eclipse_string()
 
 a.close()
 print('finish')
