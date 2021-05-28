4f658dbcf59df21b4963cb498437248bb93ce183
Михаил Бобов <bobovmikhail@mail.ru>
2021-05-28 10:46:20 +0300

    'add_finall_version'

--- lib/parser_file.py
+++ lib/parser_file.py
@@ -1,105 +1,190 @@
+import numpy as np
 import re
-
-"""
-import numpy
-import pandas
-E:/code/Parser_project/input_data/test_schedule.inc
-'W1' 10 10 1 3 OPEN 1* 1 2 1 3* 1.0 /
-'W3' 'LGR1' 10 10  2   2 	OPEN 	1* 	1	2 	1 	3* 			1.0918 /
-"""
-
-input_f = input('Введите абсолютный путь к .inc файлу: ')
-
-try:
-    f = open(input_f, 'r', encoding="utf-8")
-except:
-    print('There is no such file in this directory')
-
-a = open('file.txt', 'w', encoding="utf-8")
+import os
 
 
 def transform_schedule(keywords, parameters, input_file, output_file):
-    pass
+    """
+    reads an input file and return .csv file
+    :param keywords
+    :param parameters
+    :param input_file
+    :param output_file
+    :return:
+    """
+    return
 
 
-def read_schedule():
-    pass
+def read_schedule(file):
+    if os.path.exists(file):
+        with open(file, encoding="utf-8") as f:
+            lines = f.readlines()
+            f.close()
+            data = []
+            res = []
+            i = 0
+            for line in lines:
+                data.append(clear_file_string(line))
+            prs = [r for r in data if r not in ["", "  "]]
+            while i < len(prs):
+                if prs[i] == "WEFAC":
+                    while not prs[i] in ["DATES", "COMPDAT", "COMPDATL", "END"]:
+                        i = i + 1
+                res.append(prs[i])
+                i = i + 1
+    return res
+
+
+def clear_file_string(line):
+    char_set = set("!@#%&()[]{}/?<>")
+    output_string = ""
+    line = line[: line.find("-")]
+    for i in range(0, len(line)):
+        if not line[i] in char_set:
+            output_string += line[i]
+    return output_string
+
+
+def parse_keyword_DATE_line(current_date_line: str):
+    char_set = set("!@#%&()[]{}/?<>'")
+    output_string = ""
+    current_date_line = current_date_line[: current_date_line.find("-")]
+    for i in range(0, len(current_date_line)):
+        if not current_date_line[i] in char_set:
+            output_string += current_date_line[i]
+    parse_data = " ".join(output_string.split())
+    return parse_data
+
+
+def parse_keyword_COMPDAT_line(well_comp_line: str):
+    char_set = set("!@#%&()[]{}/?<>'")
+    output_string = ""
+    well_comp_line = well_comp_line[: well_comp_line.find("-")]
+    for i in range(0, len(well_comp_line)):
+        if not well_comp_line[i] in char_set:
+            output_string += well_comp_line[i]
+    output_string = default_params_unpacking_in_line(output_string)
+    parse_data = output_string.split()
+    parse_data.insert(1, np.nan)
+    return parse_data
+
+
+def parse_keyword_COMPDATL_line(well_comp_line: str):
+    char_set = set("!@#%&()[]{}/?<>'")
+    output_string = ""
+    well_comp_line = well_comp_line[: well_comp_line.find("-")]
+    for i in range(0, len(well_comp_line)):
+        if not well_comp_line[i] in char_set:
+            output_string += well_comp_line[i]
+    output_string = default_params_unpacking_in_line(output_string)
+    parse_data = output_string.split()
+    return parse_data
+
+
+def decode(match):
+    ch_st = set("*")
+    res = ""
+    for i in range(0, len(str(match.group(0)))):
+        if not match.group(0)[i] in ch_st:
+            res += match.group(0)[i]
+    string = ""
+    for i in range(0, int(res[0])):
+        string += " DEFAULT"
+    return str(string)
+
+
+def default_params_unpacking_in_line(well_comp_line: str):
+    output_string = re.sub("([1-9]\*)", decode, well_comp_line)
+    output_string = " ".join([el for el in output_string.split(" ") if el.strip()])
+    return output_string
 
 
 def inspect_schedule():
-    pass
+    return 1
 
 
 def clean_schedule():
-    for i in f.readlines():
-        if re.findall(r'\w+|/', i):
-            i = re.sub(r'--.*', '', i)
-            if not re.findall(r'\w+|/', i):
-                pass
-            else:
-                a.write(i)
+    return 1
 
 
-def parse_schedule():
-    pass
+def parse_schedule(list: list, keywords):
+    output = []
+    i = 0
+    while i < len(list):
+
+        if list[i] == "END":
+            break
+
+        if list[i] == "COMPDAT":
+            i = i + 1
+
+            while not list[i] in keywords:
+                res = [np.nan]
+                res.extend(parse_keyword_COMPDAT_line(list[i]))
+                output.append(res)
+                i = i + 1
+
+        if list[i] == "COMPDATL":
+            i = i + 1
+
+            while not list[i] in keywords:
+                res = [np.nan]
+                res.extend(parse_keyword_COMPDATL_line(list[i]))
+                output.append(res)
+                i = i + 1
+
+        if list[i] == "DATES":
+            i = i + 1
+
+            while not list[i + 1] in ["COMPDAT", "COMPDATL"]:
+                if list[i + 1] == "DATES":
+                    res = [parse_keyword_DATE_line(list[i]), np.nan]
+                    output.append(res)
+                    i = i + 2
+                elif list[i + 1] == "END":
+                    res = [parse_keyword_DATE_line(list[i]), np.nan]
+                    output.append(res)
+                    break
+                else:
+                    res = [parse_keyword_DATE_line(list[i]), np.nan]
+                    output.append(res)
+                    i = i + 1
+
+            date = parse_keyword_DATE_line(list[i])
+            i = i + 1
+
+            while list[i] != "DATES":
+                if list[i] == "COMPDAT":
+                    i = i + 1
+
+                    while not list[i] in keywords:
+                        res = [date]
+                        res.extend(parse_keyword_COMPDAT_line(list[i]))
+                        output.append(res)
+                        i = i + 1
+
+                if list[i] == "COMPDATL":
+                    i = i + 1
+
+                    while not list[i] in keywords:
+                        res = [date]
+                        res.extend(parse_keyword_COMPDATL_line(list[i]))
+                        output.append(res)
+                        i = i + 1
+
+                if list[i] == "END":
+                    break
+
+    return output
 
 
-def extract_keyword_blocks():
-    pass
+def extract_keywords_blocks():
+    return 1
 
 
 def extract_lines_from_keyword_block():
-    pass
+    return 1
 
 
 def parse_keyword_block():
-    pass
-
-
-def parse_keyword_DATE_line():
-    string = input('Введите строку для обработки (DATE): ')
-    out = string.split(' ')
-    print(*out[:-1])
-
-
-def parse_keyword_COMPDAT_line():
-    string = input('Введите строку для обработки (COMPDAT): ')
-    out = string.split(' ')
-    print(out[:-1])
-
-
-def parse_keyword_COMPDATL_line():
-    string = input('Введите строку для обработки (COMPDATL): ')
-    out = string.split(' ')
-    print(out[:-1])
-
-
-def format_eclipse_string():
-    s = input('Введите строку для форматирования значений DEFAULT: ')
-    old_string = s
-    def f(match):
-        return str(int(match.group(3))+1)
-
-    L = re.sub('([0-9])+', f, old_string)
-    print(L)
-
-
-def result_to_csv():
-    pass
-
-
-
-
-a = open('file.txt', encoding="utf-8")
-
-'''
-parse_keyword_DATE_line()
-parse_keyword_COMPDAT_line()
-parse_keyword_COMPDATL_line()
-'''
-
-
-format_eclipse_string()
-
-a.close()
-print('finish')
+    return 1
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from lib import parser_file
 
 
-if __name__ == '__main__':
-    keywords = ('DATES', 'COMPDAT', 'COMPDATL')
-    input_file = ''
-    output_csv = ''
-    parameters = ()
-    schedule_df = parser_file.transform_schedule()
+if __name__ == "__main__":
+    keywords = ("DATES", "COMPDAT", "COMPDATL", "END")
+    parameters = ""
+    input_file = 'input data/test_schedule.inc'
+    output_file = 'output data/schedule.csv'
 
+
+data = parser_file.read_schedule(input_file)
+print(parser_file.parse_schedule(data, keywords))
+
+i=1
